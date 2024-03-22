fd7a7938c133f4e316de83411551e406feee97dd
Naoto Kamba <naoto.kamba@outlook.jp>
2024-03-23 01:38:48 +0900

    wip

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function DashBoardPage() {
   return (
     <div>
-      <div>dashboard</div>
+      <div className="text-red-500">dashboard</div>
       <div>
         <Link to="/clients">Client 1</Link>
       </div>
